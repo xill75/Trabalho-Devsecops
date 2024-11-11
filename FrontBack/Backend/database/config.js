67bf04e1c8ef908c45a02116908ce1942ce13959
xill75 <felipeschiel@hotmail.com>
2024-11-10 23:53:52 -0300

    Removing hardcoded secrets

--- FrontBack/Backend/database/config.js
+++ FrontBack/Backend/database/config.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 module.exports = {
     DB_HOST: process.env.DB_HOST ,
-    DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || '#FpNsEr64Z2NV&*qDqvU$g*CXD',
-    DB_NAME: process.env.DB_NAME || 'malwarezin' 
+    DB_USER: process.env.DB_USER ,
+    DB_PASSWORD: process.env.DB_PASSWORD ,
+    DB_NAME: process.env.DB_NAME
 };
