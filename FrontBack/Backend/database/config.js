46d973d77d604fc47f1f317a0ecd812bc26b029c
xill75 <felipeschiel@hotmail.com>
2024-11-11 00:09:16 -0300

    Fixed secrets

--- FrontBack/Backend/database/config.js
+++ FrontBack/Backend/database/config.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 module.exports = {
     DB_HOST: process.env.DB_HOST ,
-    DB_USER: process.env.DB_USER || 'toor' ,
+    DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD ,
     DB_NAME: process.env.DB_NAME
 };
