6d1cc4a6608336e86e8faf0fcfdd1583d1019f93
xill75 <felipeschiel@hotmail.com>
2024-11-11 23:32:26 -0300

    Added Cookie

--- FrontBack/Backend/server.js
+++ FrontBack/Backend/server.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../Frontend')));
-app.use(csurf());
+app.use(csurf({cookie:true}));
 
 app.use(function(req, res, next) {
     res.cookie("mytoken", req.csrfToken());
